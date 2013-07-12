168d5e34eeea657d570fd99446e283ece9e3534a
CR <rdrgzcarlos@gmail.com>
2013-07-12 08:48:42 +0000

    Quite en web.js las comillas de data

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('data');
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
