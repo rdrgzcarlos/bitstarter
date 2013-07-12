d5821f41cf2c51cb31dfb0c082eccdc63281412a
CR <rdrgzcarlos@gmail.com>
2013-07-12 08:42:52 +0000

    Read en index.html y modifique el text Hello..

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
+var fs = require("fs");
+var data = fs.readFileSync("index.html", "utf8");
+
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('data');
 });
 
 var port = process.env.PORT || 5000;
